08303b447845e79530d8990b0ccbf52510ebec7d
Remy Porter <rp@iontank.com>
2019-02-22 17:47:41 -0500

    toneplay

--- barron.py
+++ barron.py
@@ -10,22 +10,40 @@ def play(sound, sample_rate=SAMPLE_RATE):
   sd.play(sound)
   time.sleep(len(sound) / sample_rate)
 
+def save(path, sound, sample_rate=SAMPLE_RATE):
+  sf.write(path, sound, int(sample_rate), 'FLOAT')
+
 space = sp.space(3)
 long_space = sp.space(30)
 
-pitcher = sp.saw(space, 1., np.pi/2.)*5.
 
-rising_saw_30s = np.tile(sp.reverb(
-    sp.sigmoid(space, 220+pitcher) + sp.sigmoid(space, 442-pitcher) + sp.sigmoid(space, 106-pitcher),
-    0.25, 15
-  ), 10)
-rising_saw_30s *= sp.sin(long_space, 1./15.) * sp.sin(long_space, 1./17., shift=np.pi/4.)
+def harmonic(space, base, div=2):
+  s = sp.saw(space, base)
+  for i in range(1,5*div):
+    n = i/div
+    s -= sp.saw(space, base*n) * 1./n - sp.saw(space, base/n) * 1./n
+  return sp.smooth_normalize(s)
 
-seq = np.array([125, 225, 325, 175, 90, 95]*40)
-np.random.shuffle(seq)
-melody = sp.saw(long_space, sp.arp(long_space,seq)) * np.clip(sp.square(long_space,0.425), 0., 1.) * sp.sin(long_space,0.444) * sp.sin(long_space,0.01) * 0.45
-#play(
-sf.write('working/barron.wav',
-  sp.reverb(rising_saw_30s - melody, 0.25, 8, 0.45),
-  int(SAMPLE_RATE), 'FLOAT')
-#)
+def buzz(space, f):
+  return harmonic(space, f+sp.square(space, 3)*sp.sin(space, 0.4), 3)*(sp.sigmoid(space, 0.5, np.pi) - sp.sin(space, 0.25))*sp.square(space,0.333)
+
+
+def arp_buzz(space):
+  return sp.reverb(buzz(space, sp.arp(space, [220, 440, 220, 440, 225, 440, 218])) * 0.2 +
+    buzz(space, sp.arp(space, [60, 65, 68, 64])) * sp.sin(space, 0.1) * 0.2, 0.125, 5)
+
+def shaped_noise(space):
+  return (
+    np.fmod(sp.noise(space), sp.sin(space, 60)) * 0.2 * sp.sigmoid(space, sp.sin(space, 0.45) * 0.1)
+      * sp.square(space, 1)
+  )
+
+def tone(space, seq):
+  return np.fmod(sp.saw(space, seq), sp.saw(space, seq*9.) * sp.saw(space,seq*0.2)) * sp.sin(space, 0.25) * sp.sin(space, 0.27) * sp.square(space, 1.45)
+
+save('working/rippletone.wav', tone(long_space, sp.arp(long_space, [115, 122, 104,99]*3)))
+
+play(
+  #shaped_noise(long_space) + arp_buzz(long_space)
+  tone(long_space, sp.arp(long_space, [115, 122, 104,99]*3))
+)
