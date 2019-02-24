ad4355381c041934ad4f39075b25c490e7ee981d
Remy Porter <t3knomanser@gmail.com>
2019-02-24 09:44:27 -0500

    Checkpointing composition experiments

---
+++ Notes.md
@@ -0,0 +1,5 @@
+In addition to what's in `example.py` as useful entries, as I play with this, I learn things.
+
+* convolution is a powerful way to make envelopes
+* reverb can have a negative delay time
+* Gates can be notted to control whether it's "open after" or "close after"
--- barron.py
+++ barron.py
@@ -14,7 +14,8 @@ def save(path, sound, sample_rate=SAMPLE_RATE):
   sf.write(path, sound, int(sample_rate), 'FLOAT')
 
 space = sp.space(3)
-long_space = sp.space(30)
+long_space = sp.reverb(sp.space(30), -0.25, 8, 0.77)
+total_space = sp.space(3*60)
 
 
 def harmonic(space, base, div=2):
@@ -39,11 +40,13 @@ def shaped_noise(space):
   )
 
 def tone(space, seq):
-  return np.fmod(sp.saw(space, seq), sp.saw(space, seq*9.) * sp.saw(space,seq*0.2)) * sp.sin(space, 0.25) * sp.sin(space, 0.27) * sp.square(space, 1.45)
+  return (sp.square(space, seq, shift=sp.sin(space, 0.1)*np.pi) + sp.square(space, seq+2)/2.)*np.clip(sp.square(space, 0.25), 0., 1.)*sp.sin(space, 0.25)
 
-save('working/rippletone.wav', tone(long_space, sp.arp(long_space, [115, 122, 104,99]*3)))
+#save('working/rippletone.wav', tone(long_space, sp.arp(long_space, [115, 122, 104,99]*3)))
 
+print("Starting")
 play(
-  #shaped_noise(long_space) + arp_buzz(long_space)
-  tone(long_space, sp.arp(long_space, [115, 122, 104,99]*3))
+  (sp.gate(space, 5) * (shaped_noise(long_space) + arp_buzz(long_space)) +
+    tone(long_space, sp.arp(long_space, [220,110,220,440]*9))) * 0.125
 )
+print("Done")
